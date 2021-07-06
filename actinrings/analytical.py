0fdb53897a8529a414b28d8dc71ac1e5234bbfe2
Alexander <alexandercumberworth@gmail.com>
2021-07-06 11:40:07 +0200

    Fix solver

--- actinrings/analytical.py
+++ actinrings/analytical.py
@@ -68,6 +68,11 @@ def calc_force_balanced_radius(T, X_C, kd, delta, filament_L, EI):
     return (num/denom)**(1/3)
 
 
+def calc_force_balanced_concentration(R_ring, T, kd, delta, filament_L, EI):
+    return kd*(math.exp(2*math.pi*EI*filament_L*delta /
+                        (R_ring**3*constants.k*T)) - 1)
+
+
 def calc_circular_double_overlap_ring_energy(R_ring, T, X_C, kd, delta,
                                              filament_L, N, EI):
     R_ring_max = N*filament_L/(2*math.pi)
@@ -75,13 +80,17 @@ def calc_circular_double_overlap_ring_energy(R_ring, T, X_C, kd, delta,
     sliding_energy = N*calc_sliding_energy(T, X_C, kd, delta, overlap_L)
     wlc_bending_energy = N*calc_wlc_bending_radial_energy(EI, filament_L,
                                                           R_ring)
-    return sliding_energy + wlc_bending_energy
+    total_energy = sliding_energy + wlc_bending_energy
+
+    return total_energy
 
 
-def calc_energy_minimized_radius(T, X_C, kd, delta, L, N, EI):
+def calc_energy_minimized_radius(T, X_C, kd, delta, filament_L, N, EI):
+    max_radius = N*filament_L/(2*math.pi)
     res = optimize.minimize_scalar(calc_circular_double_overlap_ring_energy,
                                    method='bounded',
-                                   bounds=(1e-30, 1),
-                                   args=(T, X_C, kd, delta, L, N, EI))
+                                   bounds=(1e-30, 2*max_radius),
+                                   args=(T, X_C, kd, delta, filament_L, N, EI),
+                                   options={'xatol': 1e-12})
 
     return res.x
