d2f68ab4a01c80c905995bc60469397cddeb38bb
Alexander <alexandercumberworth@gmail.com>
2021-07-06 12:03:55 +0200

    Implement analytical energy minimization
    I also fixed the error that this led to me discovering, that the force
    balanced expression is wrong. They are now exactly the same.

--- actinrings/analytical.py
+++ actinrings/analytical.py
@@ -62,15 +62,15 @@ def calc_wlc_bending_radial_energy(EI, filament_L, R_ring):
 
 
 def calc_force_balanced_radius(T, X_C, kd, delta, filament_L, EI):
-    num = 2*math.pi*EI*filament_L*delta
-    denom = constants.k*T*math.log(1 + X_C/kd)
+    num = EI*filament_L*delta
+    denom = 2*math.pi*constants.k*T*math.log(1 + X_C/kd)
 
     return (num/denom)**(1/3)
 
 
 def calc_force_balanced_concentration(R_ring, T, kd, delta, filament_L, EI):
-    return kd*(math.exp(2*math.pi*EI*filament_L*delta /
-                        (R_ring**3*constants.k*T)) - 1)
+    return kd*(math.exp(EI*filament_L*delta /
+                        (2*math.pi*R_ring**3*constants.k*T)) - 1)
 
 
 def calc_circular_double_overlap_ring_energy(R_ring, T, X_C, kd, delta,
@@ -85,12 +85,13 @@ def calc_circular_double_overlap_ring_energy(R_ring, T, X_C, kd, delta,
     return total_energy
 
 
-def calc_energy_minimized_radius(T, X_C, kd, delta, filament_L, N, EI):
-    max_radius = N*filament_L/(2*math.pi)
-    res = optimize.minimize_scalar(calc_circular_double_overlap_ring_energy,
-                                   method='bounded',
-                                   bounds=(1e-30, 2*max_radius),
-                                   args=(T, X_C, kd, delta, filament_L, N, EI),
-                                   options={'xatol': 1e-12})
+def calc_energy_minimized_radius(T, X_C, kd, delta, filament_L, EI):
+    num = EI*delta*filament_L
+    denom = 2*math.pi*T*constants.k*math.log(1 + X_C/kd)
 
-    return res.x
+    return (num/denom)**(1/3)
+
+
+def calc_energy_minimized_concentration(R_ring, T, kd, delta, filament_L, EI):
+    return kd*(math.exp(EI*filament_L*delta /
+                        (2*math.pi*R_ring**3*constants.k*T)) - 1)
