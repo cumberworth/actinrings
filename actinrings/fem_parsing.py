ca16b5f2fb4316bd1b258523d20deca6a581a07b
Alexander <alexandercumberworth@gmail.com>
2021-07-16 14:37:06 +0200

    Make reading in data more general

--- actinrings/fem_parsing.py
+++ actinrings/fem_parsing.py
@@ -1,16 +1,39 @@
 """Functions for parsing output from FEM calculations"""
 
 
+import itertools
+
 import pandas as pd
 
 
-def parse_integrated_output(filename):
-    """Read integrated results and return a dataframe"""
+def collect_integrated_data(filename_template, params):
 
-    return pd.read_csv(filename, sep=' ')
+    # Convert dictionary to lists of tuples of key value for key's list
+    paramsl = []
+    for key, value in params.items():
+        paramsl.append([])
+        for v in value:
+            paramsl[-1].append((key, v))
 
+    frames = []
+    for combo in itertools.product(*paramsl):
 
-def concatenate_integrated_outputs(frames):
-    """Concatenate a list of dataframes"""
+        # Convert tuple of tuples to a dictionary
+        format_dic = {}
+        for pair in combo:
+            format_dic[pair[0]] = pair[1]
+
+        filename = filename_template.format(**format_dic)
+        try:
+            frames.append(pd.read_csv(filename, sep=' '))
+        except FileNotFoundError:
+            print('Skipping {}'.format(filename))
 
     return pd.concat(frames)
+
+
+def reduce_data(data, reduce_variables):
+    for key, value in reduce_variables.items():
+        data = data[data[key] == value]
+
+    return data
